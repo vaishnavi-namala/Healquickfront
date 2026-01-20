dc28b27d60603b2cbef5466758593b7166f44b79
vaishnavi-namala <vaishnavinamala7@gmail.com>
2026-01-20 19:56:33 +0530

    Update AddDoctor.js

--- src/Admin/Dahboard/AddDoctor.js
+++ src/Admin/Dahboard/AddDoctor.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, {useState } from 'react'
 import Dashboard from './Dashboard.module.css'
 import axios from 'axios'
 import {GetApi} from '../../CallApi'
